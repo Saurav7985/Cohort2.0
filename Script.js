46ecee286c365e2ab88649d6ac435e7ce84ec9e4
Saurav7985 <sauravraikwar26@gamil.com>
2025-12-01 16:27:11 +0530

    DOM

---
+++ Script.js
@@ -0,0 +1,18 @@
+// 4 pillor od DOM -  Document Object Model
+// Selector
+// Change html
+// Change CSS
+// Event Listner
+
+let h = document.querySelector('h1')
+ let btn = document.querySelector('button')
+
+ btn.addEventListener("click",()=>{
+    
+    h.innerHTML = "You are gay!";
+    h.style.fontSize = "80px";
+    h.style.color = "red"
+    
+    btn.style.backgroundColor = "white"
+
+ })
