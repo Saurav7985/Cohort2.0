144a984f185849ac73e86b6aadd0af8fe9d4d530
Saurav7985 <sauravraikwar26@gamil.com>
2025-12-10 19:36:57 +0530

    Moving Curser

--- Script.js
+++ Script.js
@@ -1,18 +1,9 @@
+let body = document.body
 let img = document.querySelector('img')
-let main = document.querySelector("main")
-let h3 = document.querySelector('h3')
 
-// main.addEventListener("mouse")
+body.addEventListener("mousemove",(val)=>{
 
-img.addEventListener("mouseenter",function(val){
-console.log(val);
-h3.innerHTML = "chutki se dur renha..😡😡"
-h3.style.color = "black"
-main.style.backgroundColor = "red"
-})
-img.addEventListener("mouseleave",function(val){
-console.log(val);
-h3.innerHTML = "Ab dur hi rhna..😤😤"
-h3.style.color = "white"
-main.style.backgroundColor = "black"
+    img.style.left = val.x+"px";
+    img.style.top = val.y+"px";
+
 })
